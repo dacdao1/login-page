b4c68840a435ff8b72bf3fd54d4cfc94a2cb3827
DacD <dacdao01@gmail.com>
2020-09-02 16:17:51 -0400

    more update

--- src/App.vue
+++ src/App.vue
@@ -203,8 +203,8 @@ if (this.signInEmail && this.signInPassword){
             .then(res => {
               for(var i = 0; i < res.data.users.length; i++) {
       if (res.data.users[i].email === this.signInEmail && res.data.users[i].password == CryptoJS.MD5(this.signInPassword) && res.data.users[i].emailConfirmation == 1) {
-          this.$cookie.set('test', 'hello', {expires: 1, domain: 'https://test.freeingreturns.com/'});
-          window.location.href = 'https://test.freeingreturns.com/';
+          this.$cookie.set('test', 'hello', {expires: 1, domain: '/'});
+          window.location.href = 'https://setting.freeingreturns.com/';
           break;
       }
       if (res.data.users[i].email === this.signInEmail && res.data.users[i].password == CryptoJS.MD5(this.signInPassword) && res.data.users[i].emailConfirmation == 0){
