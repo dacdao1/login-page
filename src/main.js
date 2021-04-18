ebf5b57608da67e51fbb5aafb0cba0ca8573b782
DacD <dacdao01@gmail.com>
2021-04-18 18:23:16 -0400

    format js

--- src/main.js
+++ src/main.js
@@ -5,25 +5,15 @@ import App from './App.vue'
 // import 'bootstrap-vue/dist/bootstrap-vue.css';
 import VueRouter from 'vue-router'
 import VueCookie from 'vue-cookie'
-
 import './registerServiceWorker'
 import Vuelidate from 'vuelidate'
 import Popover from 'vue-js-popover'
 
-
-
 Vue.use(Vuelidate)
 Vue.use(Popover)
 Vue.config.productionTip = false
-
-
-
-
-
-
 Vue.use(VueRouter);
 Vue.use(VueCookie);
-
 
 new Vue({
   render: function (h) { return h(App) },
